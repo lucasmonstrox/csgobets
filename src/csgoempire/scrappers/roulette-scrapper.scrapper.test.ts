78a323fabf7a01a60b5da9fc7c5e9202bce15dc8
lucasmonstro <joao.galiano.silva@gmail.com>
2021-05-09 20:25:52 +0100

    ✅ add tests to isRolling method

--- src/csgoempire/scrappers/roulette-scrapper.scrapper.test.ts
+++ src/csgoempire/scrappers/roulette-scrapper.scrapper.test.ts
@@ -1,6 +1,6 @@
 import { mocked } from 'ts-jest/utils';
 import { Color } from '../../typings/roulette-scrapper.typings';
-import { getElement } from '../../utils/element';
+import { getElement, getElementOrError } from '../../utils/element';
 import RouletteScrapper from './roulette-scrapper.scrapper';
 jest.mock('../../utils/element');
 describe('RouletteScrapper', () => {
@@ -10,8 +10,8 @@ describe('RouletteScrapper', () => {
   });
   describe('getBetsTotalByColor', () => {
     it('should throw exception when cannot get bets total', () => {
-      const mockedGetElement = mocked(getElement);
-      mockedGetElement.mockImplementationOnce(
+      const mockedGetElementOrError = mocked(getElementOrError);
+      mockedGetElementOrError.mockImplementationOnce(
         () => ({ innerText: '' } as HTMLElement)
       );
       expect(() => scrapper.getBetsTotalByColor(Color.Black)).toThrow(
@@ -19,16 +19,32 @@ describe('RouletteScrapper', () => {
       );
     });
     it('should get bets total', () => {
+      const mockedGetElementOrError = mocked(getElementOrError);
+      mockedGetElementOrError.mockImplementationOnce(
+        () => ({ innerText: '10 Bets Total' } as HTMLElement)
+      );
       expect(scrapper.getBetsTotalByColor(Color.Black)).toBe(10);
     });
   });
   describe('getLast100ByColor', () => {
     it('should get last 100', () => {
-      const mockedGetElement = mocked(getElement);
-      mockedGetElement.mockImplementationOnce(
+      const mockedGetElementOrError = mocked(getElementOrError);
+      mockedGetElementOrError.mockImplementationOnce(
         () => ({ innerText: '47' } as HTMLElement)
       );
       expect(scrapper.getLast100ByColor(Color.Black)).toBe(47);
+    });
+  });
+  describe('isRolling', () => {
+    it('should not be rolling', () => {
+      const mockedGetElement = mocked(getElement);
+      mockedGetElement.mockImplementationOnce(() => null);
+      expect(scrapper.isRolling()).toBe(false);
+    });
+    it('should not be rolling', () => {
+      const mockedGetElement = mocked(getElement);
+      mockedGetElement.mockImplementationOnce(() => ({} as HTMLElement));
+      expect(scrapper.isRolling()).toBe(true);
     });
   });
 });
