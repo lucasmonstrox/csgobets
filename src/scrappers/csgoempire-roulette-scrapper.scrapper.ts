f471d66e7d4db271513d538afc4cda5251ddb6ae
lucasmonstro <joao.galiano.silva@gmail.com>
2021-05-09 17:53:27 +0100

    ✨ add method to get last100 by color

--- src/scrappers/csgoempire-roulette-scrapper.scrapper.ts
+++ src/scrappers/csgoempire-roulette-scrapper.scrapper.ts
@@ -1,13 +1,18 @@
 import { Color } from '../typings/roulette-scrapper.typings';
 import { getElement } from '../utils/element';
 export default class CSGOEmpireRouletteScrapper {
-  colorQuerySelectorMapper: Record<Color, string> = {
+  betsTotalQuerySelectorMapper: Record<Color, string> = {
     black: '.bets-container:first-child',
     green: '.bets-container:nth-child(2)',
     red: '.bets-container:last-child',
   };
+  last100QuerySelectorMapper: Record<Color, string> = {
+    black: '.text-light-grey-1.text-xxs.font-bold.mr-2:first-child',
+    green: '.text-light-grey-1.text-xxs.font-bold.mr-2:nth-child(2)',
+    red: '.text-light-grey-1.text-xxs.font-bold.mr-2:last-child',
+  };
   getBetsTotalByColor(color: Color): number {
-    const querySelector = this.colorQuerySelectorMapper[color];
+    const querySelector = this.betsTotalQuerySelectorMapper[color];
     const betsTotalElement = getElement(querySelector);
     const match = betsTotalElement.innerText.match(
       /(?<betsTotal>\d+) Bets Total/
@@ -18,5 +23,11 @@ export default class CSGOEmpireRouletteScrapper {
     }
     const betsTotal = +match!.groups!.betsTotal;
     return betsTotal;
+  }
+  getLast100ByColor(color: Color): number {
+    const querySelector = this.last100QuerySelectorMapper[color];
+    const element = getElement(querySelector);
+    const last100 = +element.innerText;
+    return last100;
   }
 }
