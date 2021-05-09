e18c96bdab4b308edce25a9523901704fc95541d
lucasmonstro <joao.galiano.silva@gmail.com>
2021-05-09 20:39:01 +0100

    🔥 remove unused type last100

--- src/typings/roulette-scrapper.typings.ts
+++ src/typings/roulette-scrapper.typings.ts
@@ -3,11 +3,6 @@ export enum Color {
   Green = 'green',
   Red = 'red',
 }
-export interface Last100 {
-  black: number;
-  green: number;
-  red: number;
-}
 export interface RouletteScrapper {
   getBetsTotalByColor(color: Color): number;
   getLast100ByColor(color: Color): number;
