52560da155b59fa8cfae960039dcd71980740b50
lucasmonstro <joao.galiano.silva@gmail.com>
2021-05-09 17:55:24 +0100

    ✅ add test to getLast100ByColor

--- src/scrappers/csgoempire-roulette-scrapper.scrapper.test.ts
+++ src/scrappers/csgoempire-roulette-scrapper.scrapper.test.ts
@@ -8,16 +8,27 @@ describe('CSGOEmpireRouletteScrapper', () => {
   beforeEach(() => {
     scrapper = new CSGOEmpireRouletteScrapper();
   });
-  it('should throw exception when cannot get bets total', () => {
-    const mockedGetElement = mocked(getElement);
-    mockedGetElement.mockImplementationOnce(
-      () => ({ innerText: '' } as HTMLElement)
-    );
-    expect(() => scrapper.getBetsTotalByColor(Color.Black)).toThrow(
-      'Cannot extract regex "\\d+ Bets Total" from content'
-    );
+  describe('getBetsTotalByColor', () => {
+    it('should throw exception when cannot get bets total', () => {
+      const mockedGetElement = mocked(getElement);
+      mockedGetElement.mockImplementationOnce(
+        () => ({ innerText: '' } as HTMLElement)
+      );
+      expect(() => scrapper.getBetsTotalByColor(Color.Black)).toThrow(
+        'Cannot extract regex "\\d+ Bets Total" from content'
+      );
+    });
+    it('should get bets total', () => {
+      expect(scrapper.getBetsTotalByColor(Color.Black)).toBe(10);
+    });
   });
-  it('should get bets total', () => {
-    expect(scrapper.getBetsTotalByColor(Color.Black)).toBe(10);
+  describe('getLast100ByColor', () => {
+    it('should get last 100', () => {
+      const mockedGetElement = mocked(getElement);
+      mockedGetElement.mockImplementationOnce(
+        () => ({ innerText: '47' } as HTMLElement)
+      );
+      expect(scrapper.getLast100ByColor(Color.Black)).toBe(47);
+    });
   });
 });
