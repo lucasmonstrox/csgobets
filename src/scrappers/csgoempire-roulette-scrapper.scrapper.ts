0d01b5a938dcdde51ca78b354f2a2fcc15a4ab0b
lucasmonstro <joao.galiano.silva@gmail.com>
2021-05-09 20:01:16 +0100

    ✨ add method to getPreviousRolls

--- src/scrappers/csgoempire-roulette-scrapper.scrapper.ts
+++ src/scrappers/csgoempire-roulette-scrapper.scrapper.ts
@@ -1,5 +1,5 @@
 import { Color } from '../typings/roulette-scrapper.typings';
-import { getElement } from '../utils/element';
+import { getElement, getElements } from '../utils/element';
 export default class CSGOEmpireRouletteScrapper {
   betsTotalQuerySelectorMapper: Record<Color, string> = {
     black: '.bets-container:first-child',
@@ -11,6 +11,7 @@ export default class CSGOEmpireRouletteScrapper {
     green: '.text-light-grey-1.text-xxs.font-bold.mr-2:nth-child(2)',
     red: '.text-light-grey-1.text-xxs.font-bold.mr-2:last-child',
   };
+  previousRollsSelector = '.previous-rolls-item > div';
   getBetsTotalByColor(color: Color): number {
     const querySelector = this.betsTotalQuerySelectorMapper[color];
     const betsTotalElement = getElement(querySelector);
@@ -29,5 +30,22 @@ export default class CSGOEmpireRouletteScrapper {
     const element = getElement(querySelector);
     const last100 = +element.innerText;
     return last100;
+  }
+  // TODO: test it
+  getPreviousRolls(): Color[] {
+    const previousRollsElements = getElements(this.previousRollsSelector);
+    const previousRolls = [];
+    for (let i = 0; i < 10; i++) {
+      const rollElement = previousRollsElements[i];
+      const isBlack = rollElement.className.indexOf(`coin-ct`) > -1;
+      if (isBlack) {
+        previousRolls.push(Color.Black);
+        continue;
+      }
+      const isRed = rollElement.className.indexOf(`coin-t`) > -1;
+      const rollResult = isRed ? Color.Red : Color.Green;
+      previousRolls.push(rollResult);
+    }
+    return previousRolls;
   }
 }
