11a7ac14988864ae69c38ab3a7234f5e7a796b9d
lucasmonstro <joao.galiano.silva@gmail.com>
2021-06-04 22:15:55 +0100

    🚚 change roulette scrapper to repository suffix

--- src/csgoempire/repositories/roulette/roulette.repository.test.ts
+++ src/csgoempire/repositories/roulette/roulette.repository.test.ts
@@ -1,7 +1,7 @@
 import { mocked } from 'ts-jest/utils';
 import { Color } from '../../../typings/roulette-scrapper.typings';
 import { getElement, getElementOrError } from '../../../utils/element';
-import RouletteScrapper from './roulette.scrapper';
+import RouletteScrapper from './roulette.repository';
 jest.mock('../../../utils/element');
 describe('RouletteScrapper', () => {
   let scrapper: RouletteScrapper;
